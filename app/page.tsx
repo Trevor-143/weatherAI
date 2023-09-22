13dba2d56d55502eb843e12c1b4ecbfe04e41182
Trevor_143 <trevorarapu2@gmail.com>
2023-09-22 16:39:06 +0300

    finished card

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 "use client"
 
+import { Card, Divider, Subtitle, Text } from "@tremor/react"
+
 export default function Home() {
   return (
-    <main className="text-6xl" >
-      <h1>Lets build a weather App</h1>
-    </main>
+    <div className="min-h-screen bg-gradient-to-br from-[#165f4d] to-[#044a53] p-10 flex flex-col justify-center items-center ">
+      <Card className=" max-w-4xl mx-auto " >
+        <Text className=" text-5xl font-bold text-center mb-10" >Weather AI</Text>
+        <Subtitle className="text-xl text-center" >Powered by NextJS</Subtitle>
+
+        <Divider className="my-10" />
+        <Card className = "bg-gradient-to-br from-[#165f4d] to-[#044a53]" >
+          {/* cityPicker */}
+        </Card>
+      </Card>
+    </div>
   )
 }
